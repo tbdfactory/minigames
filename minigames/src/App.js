b3d00c9dcf59046d2e306128fd2e7b00fc930f95
Miguel <miguelguibarra@gmail.com>
2022-09-30 01:03:03 -0300

    #004 - Create Topbar Component (CSS)

--- minigames/src/App.js
+++ minigames/src/App.js
@@ -2,11 +2,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/reset.css";
 import "./styles/App.css";
 import Home from "./components/Home";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="page">
-      <nav className="topbar"></nav>
+      <NavBar />
       <Home />
     </div>
   );
---
+++ minigames/src/components/NavBar.js
@@ -0,0 +1,43 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/NavBar.css";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import logo from "../images/favicon.png";
+import { useState } from "react";
+
+function NavBar() {
+  const [isActive, setIsActive] = useState(false);
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
+  return (
+    <header>
+      <nav className="nav">
+        <section className="nav-container container">
+          <div onClick={handleClick} className={`${isActive ? "active" : ""} hamburger`}>
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </div>
+          <figure className="figure">
+            <img className="minigame-logo" src={logo}></img>
+            <h3>minigames</h3>
+          </figure>
+          <div className={`${isActive ? "active" : ""} nav-links`}>
+            <p className="m-0 tag-name">Hi, Miguel!</p>
+            <div className="d-flex">
+              <i className="padding logo-moon">
+                <DarkModeIcon color="white" fontSize="large"></DarkModeIcon>
+              </i>
+              <i className="padding navbar-icon">
+                <GitHubIcon color="white" fontSize="inherit"></GitHubIcon>
+              </i>
+            </div>
+          </div>
+        </section>
+      </nav>
+    </header>
+  );
+}
+
+export default NavBar;
